34b3c6c665b9bfdeb94a8ececa67920e1b9c42ff
Vasilii_Pozdeev <vaspoz@mail.ru>
2017-02-15 19:55:52 +0100

    Fetch cities list by chosen country

--- src/app/main.js
+++ src/app/main.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import AutoComplete from 'material-ui/AutoComplete';
+import Ajax from '../Connector';
 
 const styles = {
   container: {
@@ -13,10 +14,27 @@ export class Main extends Component {
   constructor(props, context) {
     super(props, context);
 
-    console.log(this.props.countriesList);
+    this.handleCountrySelection = this.handleCountrySelection.bind(this);
+    this.fetchCitiesList = this.fetchCitiesList.bind(this);
+
     this.state = {
-      countries: this.props.countriesList
+      countries: this.props.countriesList,
+      citiesControlEnable: false,
+      citiesReceived: []
     };
+  }
+
+  fetchCitiesList(data) {
+    this.setState({
+      citiesReceived: JSON.parse(data),
+      citiesControlEnable: true
+    });
+  }
+
+  handleCountrySelection(countrySelected) {
+    const ajax = new Ajax();
+    const link = 'http://localhost:8080/cities/by/' + countrySelected;
+    ajax.getRequest(link, this.fetchCitiesList);
   }
 
   render() {
@@ -28,13 +46,15 @@ export class Main extends Component {
           filter={AutoComplete.caseInsensitiveFilter}
           dataSource={this.props.countriesList}
           maxSearchResults={10}
+          onNewRequest={this.handleCountrySelection}
         />
         <br/>
         <AutoComplete
           floatingLabelText="City"
           filter={AutoComplete.caseInsensitiveFilter}
-          dataSource={['aaa', 'bbb', 'ccc']}
-          maxSearchResults={5}
+          dataSource={this.state.citiesReceived}
+          maxSearchResults={10}
+          disabled={!this.state.citiesControlEnable}
         />
         <br/>
         <br/>
