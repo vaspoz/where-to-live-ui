ab3e58228dcf2b2c06736b8f48512e64856583d3
Vasilii_Pozdeev <vaspoz@mail.ru>
2017-02-24 13:41:05 +0100

    Sort country list in the store

--- src/components/ajax/api.js
+++ src/components/ajax/api.js
@@ -4,13 +4,20 @@ const port = 8080;
 const baseURL = 'http://localhost:' + port;
 const countriesURL = baseURL + '/countries';
 const citiesURL = baseURL + '/cities/by/';
+const chartsURL = baseURL + '/compare';
 
 const BEapi = {
 	getCountryList: () => {
-		return fetch(countriesURL).then(response => response.json()).then(countryList => countryList.sort());
+		return fetch(countriesURL)
+			.then(response => response.json())
+			.then(countryList => countryList.sort());
 	},
 	getCityList: (countryName) => {
 		return fetch(citiesURL + countryName).then(response => response.json());
+	},
+	getCharts: (baseCountry, baseCity, countryList) => {
+		return fetch(`${chartsURL}/${baseCountry}/${baseCity}/with-all/${countryList.join(',')}`)
+			.then(response => response.json());
 	}
 };
 
