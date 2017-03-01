5dbe92474792a8b518a6958be8ee9e3fd5380599
Vasilii_Pozdeev <vaspoz@mail.ru>
2017-03-01 16:48:12 +0100

    Create simple (== empty) header

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,27 +1,29 @@
 import React, {PropTypes} from 'react';
 import RefreshIndicator from 'material-ui/RefreshIndicator';
+import Paper from 'material-ui/Paper';
 
-// const style = {
-// 	container: {
-// 		position: 'relative'
-// 	},
-// 	refresh: {
-// 		display: 'inline-block',
-// 		position: 'relative'
-// 	}
-// };
+const style = {
+	headerContainer: {
+		height: 'inherit',
+		textAlign: 'center'
+	},
+	refresh: {
+		display: 'inline-block',
+		position: 'relative'
+	}
+};
 
 const Header = ({loading}) => {
 	return (
-		<div>
-			Header is coming.....
+		<Paper zDepth={2} style={style.headerContainer}>
 			<RefreshIndicator
 				left={0}
 				top={0}
 				size={25}
-				status={loading ? 'loading' : 'hide'}
+				style={style.refresh}
+				status={loading? 'loading': 'hide'}
 			/>
-		</div>
+		</Paper>
 	);
 };
 
