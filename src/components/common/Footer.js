47adee56fa12ea40f0ac1a629c1ca58d7de9f564
Vasilii_Pozdeev <vaspoz@mail.ru>
2017-03-01 16:06:33 +0100

    Create simple footer

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,10 +1,19 @@
 import React, {PropTypes} from 'react';
+import Paper from 'material-ui/Paper';
+
+const style = {
+	paper: {
+		height: 'inherit'
+	}
+};
 
 const Footer = () => {
 	return (
-		<div>
-			Footer is coming...
-		</div>
+		<Paper zDepth={5} style={style.paper}>
+			<div className="footer-text">
+				Made with ♥ by Vasilii Pozdeev in Gdansk 2017
+			</div>
+		</Paper>
 	);
 };
 
