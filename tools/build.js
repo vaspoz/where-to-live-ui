c8ad88b32c31195d4241ba97a2c2598572d16940
vaspoz <vaspoz@mail.ru>
2020-01-08 15:58:12 +0100

    Changed the port for production deployment

--- tools/build.js
+++ tools/build.js
@@ -5,7 +5,7 @@ import colors from 'colors';
 
 process.env.NODE_ENV = 'production'; // this assures the Babel dev config (for hot reloading) doesn`t apply
 
-console.log('Generating minified bundle for productio via Webpack. This will take a moment...'.blue);
+console.log('Generating minified bundle for production via Webpack. This will take a moment...'.blue);
 
 webpack(webpackConfig).run((err, stats) => {
 	if (err) {
